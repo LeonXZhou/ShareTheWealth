a224a1b986a564a250f45513c205f257a19d696f
SatanshuMishra <63601536+SatanshuMishra@users.noreply.github.com>
2022-02-05 15:40:56 -0800

    Version 0.2

---
+++ sharethewealth/src/Pages/CreateEvent.jsx
@@ -0,0 +1,25 @@
+import "./CreateEvent.css";
+import { useState } from "react";
+export default function CreateEvent() {
+    const [formState, setFormState] = useState({ name:"", date:"" });
+    return (
+        <div>
+            <h1>Create Event</h1>
+            <form>
+                <input type={"text"} value={formState.name} onSubmit={(e)=>{e.preventDefault()}} onChange={(e) => {
+                    setFormState((prev) => {
+                        const newState = { ...prev, name: e.target.value }
+                        return newState;
+                    })
+                }}></input>
+                <input type={"date"} value={formState.date} onChange={(e) => {
+                    setFormState((prev) => {
+                        const newState = { ...prev, date: e.target.value }
+                        return newState;
+                    })
+                }}></input>
+                <button onClick> Create </button>
+            </form>
+        </div>
+    );
+}
