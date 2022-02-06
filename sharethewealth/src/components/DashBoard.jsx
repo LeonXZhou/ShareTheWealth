2a3a32c47cb8df0d356d30f3a6d1e8dee4c2a45e
SatanshuMishra <63601536+SatanshuMishra@users.noreply.github.com>
2022-02-05 22:46:26 -0800

    Update DashBoard.jsx

--- sharethewealth/src/components/DashBoard.jsx
+++ sharethewealth/src/components/DashBoard.jsx
@@ -1,1 +1,29 @@
-export default DashBoard
+import OcassionItem from "./OccassionItem"
+import "./Dashboard.css"
+
+
+
+export default function Dashboard() {
+    const data = [
+        {
+            id: 1,
+            name: "Friday Party",
+            date: "2022-01-22"
+        },
+        {
+            id: 2,
+            name: "Saint Patty's",
+            date: "2022-01-23"
+        }
+    ]
+    const occassions = data.map((occassion) => {
+        return (<OcassionItem name={occassion.name} date={occassion.date} key ={occassion.id}></OcassionItem>)
+    })
+
+    return (<>
+    <div className={"dash"}>
+        <h1>upcoming events</h1>
+        {occassions}
+    </div>
+    </>)
+}
