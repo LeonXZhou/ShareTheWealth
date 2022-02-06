7738f72749f4cea3a7cfd026415698fc0874af9f
LeonXZhou <leon.zhou@queensu.ca>
2022-02-06 13:19:28 -0500

    added some implementation for title

--- sharethewealth/src/components/Title.jsx
+++ sharethewealth/src/components/Title.jsx
@@ -1,7 +1,11 @@
 import "./Title.css";
+import Cookies from "js-cookie";
 function Title(){
     return(
-        <p className="title">ShareTheWealth</p>
+        <div className="nav">
+            <div className="title">ShareTheWealth</div>
+            <div>TEST USER {Cookies.get("user_id")}</div>
+        </div>
     );
 }
 export default Title;
